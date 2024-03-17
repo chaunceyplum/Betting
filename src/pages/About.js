07c7dcb4b3d3c73ede50c2e2a1761bd14e255616
Chauncey <Chaunceyplum@gmail.com>
2024-03-17 14:12:42 -0400

    added content for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,73 @@
+import Footer from '@/components/Footer'
+import MyNav from '@/components/MyNav'
+import Reasons from '@/components/Reasons'
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const About = () => {
   return (
     <div>
-      <div>About</div>
+      <MyNav />
+      <Container className='text-white'>
+        <br />
+        <br />
+        <br />
+        <br />
+        <h3 className='text-center'>About BetMaster Pro</h3>
+        <br />
+        <br />
+        <br />
+        <br />
+        <div>
+          <p>
+            BetMaster Pro is a revolutionary betting predictions application
+            dedicated to enhancing your sports betting experience. Founded by a
+            team of passionate sports enthusiasts and betting experts, our
+            mission is to provide users with the tools and insights needed to
+            make informed betting decisions and maximize their winnings.
+          </p>
+          <h4>Our Mission</h4>
+          <p>
+            At BetMaster Pro, we understand the challenges that sports bettors
+            face in a highly competitive and unpredictable market. Thats why
+            were committed to leveraging cutting-edge technology and expert
+            analysis to deliver accurate predictions across a wide range of
+            sports and events.
+          </p>
+          <h4>What Sets Us Apart</h4>
+          <p>
+            Unlike other betting prediction platforms, BetMaster Pro combines
+            the power of advanced algorithms with the expertise of seasoned
+            betting professionals. Our team meticulously analyzes historical
+            data, player statistics, team performance, and other relevant
+            factors to generate predictions that are second to none.
+          </p>
+          <h4>Our Vision</h4>
+          <p>
+            Our vision is to become the go-to platform for sports bettors
+            worldwide, offering unparalleled accuracy, reliability, and user
+            experience. We strive to empower our users with the knowledge and
+            confidence to succeed in their betting endeavors while promoting
+            responsible gambling practices.
+          </p>
+          <h4>Join Us Today</h4>
+          <p>
+            Whether youre a casual bettor looking to improve your odds or a
+            seasoned veteran aiming for consistent wins, BetMaster Pro is here
+            to help you achieve your goals. Join our community today and take
+            your betting game to the next level!
+          </p>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+      </Container>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer />
     </div>
   )
 }
