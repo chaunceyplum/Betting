42d9f926e3cfe5e806e6f17c5a6980428b28d935
Chauncey <Chaunceyplum@gmail.com>
2024-03-17 20:28:11 -0400

    fixed display player page and added search bar to search for players

--- src/pages/SearchPlayer.js
+++ src/pages/SearchPlayer.js
@@ -1,25 +1,25 @@
-const SearchPlayer = () => {
-  return (
-    <div>
-      <div>SearchPlayer</div>
-      <input
-        type='text'
-        onChange={(event) => setSearch(event.target.value)}
-        className=''
-        placeholder='Search by name'
-      />
-      {props.data.data.filter((cur) => {
-        if (search == '') {
-          return cur
-        } else if (
-          cur.name.toLowerCase().includes(search.toLowerCase()) ||
-          cur.symbol.toLowerCase().includes(search.toLowerCase())
-        ) {
-          return cur
-        }
-      })}
-    </div>
-  )
-}
+// const SearchPlayer = () => {
+//   return (
+//     <div>
+//       <div>SearchPlayer</div>
+//       <input
+//         type='text'
+//         onChange={(event) => setSearch(event.target.value)}
+//         className=''
+//         placeholder='Search by name'
+//       />
+//       {props.data.data.filter((cur) => {
+//         if (search == '') {
+//           return cur
+//         } else if (
+//           cur.name.toLowerCase().includes(search.toLowerCase()) ||
+//           cur.symbol.toLowerCase().includes(search.toLowerCase())
+//         ) {
+//           return cur
+//         }
+//       })}
+//     </div>
+//   )
+// }
 
-export default SearchPlayer
+// export default SearchPlayer
